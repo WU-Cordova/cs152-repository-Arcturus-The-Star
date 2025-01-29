851e740b3cd3f5ad2ebfd68381ae3ed3d534efa8
Arcturus-The-Star <sjavery@willamette.edu>
2025-01-29 15:47:28 -0800

    Modified Card methods

--- projects/project1/Card.py
+++ projects/project1/Card.py
@@ -1,9 +1,13 @@
 from datastructures.bag import Bag
 
 class Card:
-    def __init__(self, suit, value):
+    def __init__(self, suit, name):
         self.__suit = suit
-        self.__value = value
+        self.__value = name if type(name) == int else 11 if name == "A" else 10
+        self.__name = name
+
+    def __str__(self):
+        return f"{self.__suit} {self.__name}"
 
     @property
     def suit(self) -> str:
@@ -13,10 +17,18 @@ class Card:
     def value(self) -> int:
         return self.__value
 
+    @value.setter
+    def value(self, value):
+        self.__value = value
+
+    @property
+    def name(self):
+        return self.__name
+
 class MultiDeck:
     def __init__(self, num):
         self.__deck = Bag()
-        if num not in [2,4,6,8]:
+        if num not in {2,4,6,8}:
             raise ValueError("MultiDeck only supports deck numbers of 2, 4, 6, or 8")
         self.create_deck()
         for num in range(num - 1):
@@ -24,8 +36,8 @@ class MultiDeck:
                 self.__deck.add(card)
 
     def create_deck(self) -> None:
-        for suit in ["Hearts", "Diamonds", "Clubs", "Spades"]:
-            for value in ["Ace", "Jack", "Queen", "King"] + list(range(1,11)):
+        for suit in [chr(9829), chr(9830), chr(9827), chr(9824)]:
+            for value in ["A", "J", "Q", "K"] + list(range(1,11)):
                 self.__deck.add(Card(suit, value))
 
     @property
