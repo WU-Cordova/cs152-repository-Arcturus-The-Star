36b0f9489a2d40a428757bb2f46f2f380907b347
Arcturus-The-Star <sjavery@willamette.edu>
2025-01-31 11:28:15 -0800

    implemented game logic

--- Class/BattleDice/game.py
+++ Class/BattleDice/game.py
@@ -1,4 +1,4 @@
-import random
+import random, time
 from character import Character
 
 class Game:
@@ -11,12 +11,39 @@ class Game:
         """
         self.__player1 = player1
         self.__player2 = player2
+        self.__player_list = [self.__player1, self.__player2]
 
     def attack(self, attacker: Character, defender: Character):
-        pass # TODO: Implement die roll 1-6 and apply scaled attack power to the defender
+        """
+        Performs an attack where the attacker rolls a die to determine damage dealt
+        """
+        defender.health -= (damage := ((roll := random.randint(1,6)) * attacker.attack_power))
+        print(f"{attacker.name} attacked {defender.name} with a roll of {5}. {defender.name} took {damage} damage")
+
+    def check_win(self):
+        if self.__player1.health < 0:
+            print(f"{self.__player2.name} has won!")
+            return True
+        elif self.__player2.health < 0:
+            print(f"{self.__player1.name} has won!")
+            return True
+        else:
+            return False
 
     def start_battle(self):
         """
         Starts a turn based battle between the two players
         """
-        pass # TODO: Implement the battle loop where players take turns attacking
+        in_battle = True
+        starter = random.randint(0,1)
+        while in_battle:
+            self.attack(self.__player_list[starter], self.__player_list[1 - starter])
+            if win := self.check_win():
+                in_battle = not win
+                break
+            time.sleep(.2)
+            self.attack(self.__player_list[1 - starter], self.__player_list[starter])
+            if win := self.check_win():
+                in_battle = not win
+                break
+            time.sleep(.2)
