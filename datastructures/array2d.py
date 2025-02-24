9b36dbda5a2e3139717f59f32a944ae4aa9c264e
Arcturus-The-Star <sjavery@willamette.edu>
2025-02-24 11:10:30 -0800

    implemented various methods to array2d

--- datastructures/array2d.py
+++ datastructures/array2d.py
@@ -17,21 +17,26 @@ class Array2D(IArray2D[T]):
             self.__array = array
 
         def __getitem__(self, column_index: int) -> T:
-            return self.__array[(self.__row_index * self.__num_columns) + column_index]
+            if column_index >= self.__num_columns:
+                raise IndexError("Column index out of bounds")
+            else:
+                return self.__array[(self.__row_index * self.__num_columns) + column_index]
         
         def __setitem__(self, column_index: int, value: T) -> None:
-            if isinstance(value, self.__data_type):
-                self.__array[(self.__row_index * self.__num_columns) + column_index] = value
-            else:
+            if column_index >= self.__num_columns:
+                raise IndexError("Column index out of bounds")
+            elif not isinstance(value, self.__data_type):
                 raise ValueError(f"This array only accepts data of type {self.__data_type}")
+            else:
+                self.__array[(self.__row_index * self.__num_columns) + column_index] = value
         
         def __iter__(self) -> Iterator[T]:
-            for i in range(0, self.__num_columns):
-                yield self[i]
+            for column_index in range(self.__num_columns):
+                yield self[column_index]
         
         def __reversed__(self) -> Iterator[T]:
-            for i in range(self.__num_columns - 1, 0):
-                yield self[i]
+            for column_index in range(self.__num_columns-1, -1, -1):
+                yield self[column_index]
 
         def __len__(self) -> int:
             return self.__num_columns
@@ -48,10 +53,10 @@ class Array2D(IArray2D[T]):
             raise ValueError("Starting_sequence should be a sequence")
         if not isinstance(data_type, type):
             raise ValueError("Data_type should be a type")
-        unpacked_sequence = []
         self.__data_type = data_type
         self.__num_columns = len(starting_sequence[0])
         self.__num_rows = len(starting_sequence)
+        unpacked_sequence = []
         for row in starting_sequence:
             if not isinstance(row, Sequence):
                 raise ValueError("Starting_sequence should contain sequences")
@@ -61,25 +66,29 @@ class Array2D(IArray2D[T]):
                 for i in row:
                     if not isinstance(i, data_type):
                         raise ValueError(f"All elements of starting_sequence should be of type {data_type}")
-                    unpacked_sequence.append(i)
+                    else:
+                        unpacked_sequence.append(i)
         self.__array = Array(unpacked_sequence, data_type)
 
     @staticmethod
     def empty(rows: int=0, cols: int=0, data_type: type=object) -> Array2D:
         return Array2D([[data_type() for _ in range(cols)] for _ in range(rows)], data_type)
 
-    def __getitem__(self, row_index: int) -> Array2D.IRow[T]: 
-        return self.Row(row_index, self.__array, self.__num_columns, self.__data_type)
+    def __getitem__(self, row_index: int) -> Array2D.IRow[T]:
+        if row_index >= self.__num_rows:
+            raise IndexError("Row index out of bounds")
+        else:
+            return self.Row(row_index, self.__array, self.__num_columns, self.__data_type)
     
     def __iter__(self) -> Iterator[Sequence[T]]: 
-        for i in range(0, self.__num_rows):
-            yield iter(self.Row(i, self.__array, self.__num_columns, self.__data_type))
+        for row_index in range(self.__num_rows):
+            yield self[row_index]
     def __reversed__(self):
-        for i in range(self.__num_rows - 1, 0):
-            yield iter(self.Row(i, self.__array, self.__num_columns, self.__data_type))
+        for row_index in range(self.__num_rows-1, -1, -1):
+            yield self[row_index]
     
     def __len__(self): 
-        raise NotImplementedError('Array2D.__len__ not implemented')
+        return self.__num_rows
                                   
     def __str__(self) -> str: 
         return f'[{", ".join(f"{str(row)}" for row in self)}]'
@@ -91,4 +100,5 @@ class Array2D(IArray2D[T]):
 if __name__ == '__main__':
     filename = os.path.basename(__file__)
     #print(f'This is the {filename} file.\nDid you mean to run your tests or program.py file?\nFor tests, run them from the Test Explorer on the left.')
-    print(repr(Array2D([[1,2,3], [4,5,6], [7,8,9]], int)))
+    ar = Array2D([[1,2,3],[4,5,6],[7,8,9]], int)
+    print(repr(ar))
