e9aa177ba112f8ca9eb5a9a0774ad4530c1588b3
Arcturus-The-Star <sjavery@willamette.edu>
2025-02-26 19:06:29 -0800

    completed grid and changed cell to be compatible

--- projects/project2/Cell.py
+++ projects/project2/Cell.py
@@ -1,10 +1,21 @@
 class Cell:
-    def __init__(self, status:bool):
+    def __init__(self, coords:tuple, status:bool = False):
+        """
+        A single cell of the game of life
+        Parameters:
+            coords: The coordinates of the cell, in (Row, Column) form
+            status: True if the cell is living, False if it is dead. Defaults to False
+        """
         if not isinstance(status, bool):
             raise TypeError("Status should be a boolean")
         self.__neighbors:int = 0 # Live neighbors
         self.__will_be_alive:bool = status
         self.__living:bool = status
+        self.__coordinates:tuple = coords
+
+    @property
+    def coordinates(self) -> tuple:
+        return self.__coordinates
 
     @property
     def living(self) ->bool:
@@ -32,3 +43,15 @@ class Cell:
 
     def update_life(self):
         self.__living = self.__will_be_alive
+
+    def __str__(self):
+        if self.living:
+            return "\u25A1"
+        else:
+            return "\u25A0"
+
+    def __repr__(self):
+        return f"{"Living" if self.living else "Dead"} Cell at Row:{self.coordinates[0]}, Column:{self.coordinates[1]} with {self.neighbors} neighbors, predicted to be {"Living" if self.__will_be_alive else "Dead"}"
+
+    def __eq__(self, other):
+        return self.living == other.living
--- projects/project2/Grid.py
+++ projects/project2/Grid.py
@@ -1,14 +1,61 @@
 from Cell import Cell
 from datastructures.array2d import Array2D
 
-def random_permutation():
-    pass
 
 class Grid(Array2D):
-    def __init__(self, input_permutation=random_permutation()):
-        super(Grid, self).__init__(input_permutation, Cell)
+
+    def __init__(self, input_permutation:list[list[bool]]):
+        super(Grid, self).__init__([[Cell((j,i), input_permutation[j][i]) for i in range(len(input_permutation[0]))] for j in range(len(input_permutation))], Cell)
         self.__living_cells = 0
+        self.count_living()
 
     @property
     def living_cells(self):
         return self.__living_cells
+
+    def count_living(self):
+        counter = 0
+        for i in self:
+            for j in i:
+                if j.living:
+                    counter += 1
+        self.__living_cells = counter
+
+    def update_neighbors(self, row, col) ->int:
+        neighbors = []
+        for i in range(-1,2):
+            for j in range(-1,2):
+                if  (0 <= row + i < len(self) and 0 <= col + j < self.width) and not (i == 0 and j == 0):
+                    neighbors.append(self[row + i][col + j].living)
+        self[row][col].neighbors = neighbors.count(True)
+
+    def update_all_neighbors(self):
+        for row in range(len(self)):
+            for col in range(self.width):
+                self.update_neighbors(row,col)
+
+    def predict_all_cells(self):
+        for row in range(len(self)):
+            for col in range(self.width):
+                self[row][col].prospective_life()
+
+    def update_all_cells(self):
+        for row in range(len(self)):
+            for col in range(self.width):
+                self[row][col].update_life()
+
+    def __str__(self):
+        output = ""
+        for i in range(len(self)):
+            for j in range(self.width):
+                output += str(self[i][j]) + " "
+            output += "\n"
+        return output
+
+    def __repr__(self):
+        return f'Grid {self.width} Rows x {len(self)} Columns, living cells: {self.__living_cells}, items:\n{str(self)}'
+
+
+
+if __name__ == "__main__":
+    gr = Grid([[False,False,True,False,False],[False,False,True,False,False],[False,False,True,False,False]])
