94c5441e4de382b46211d6d67854541dc9aad7a5
Arcturus-The-Star <sjavery@willamette.edu>
2025-02-26 21:42:55 -0800

    added invalid config and also finished gamecontroller

--- projects/project2/GameController.py
+++ projects/project2/GameController.py
@@ -1,4 +1,3 @@
-from Cell import Cell
 from Grid import Grid
 from random import getrandbits
 import os, time, copy
@@ -13,82 +12,96 @@ class GameController:
         self.__manual_mode:bool = False
 
     def start(self):
-        print("Welcome to the Game of Life.")
-        while board_state:= input("To begin, press l to load a file, or r to generate a random board.\n>") + " ":
-            match board_state.lower().strip():
-                case "l":
-                    print("Load chosen.\nPlease enter the name of the configuration file.")
-                    files = [f[:f.find(".")].upper() for f in os.listdir("GOL_Configs") if os.path.isfile(os.path.join("GOL_Configs", f))]
-                    [print("  - " + line) for line in files]
-                    while filename := input("> ") + " ":
-                        if filename.rstrip().upper() in files:
-                            board = self.read_config(filename.rstrip())
-                            break
-                        else:
-                            print("Not a valid configuration file.")
-                    break
-                case "r":
-                    print("Random chosen.\nPlease input your dimensions:")
-                    while rows := input("Rows:\n>") + " ":
-                        try:
-                            int(rows)
-                            break
-                        except ValueError:
-                            print("Rows should be an integer")
-                    while cols := input("Columns:\n>") + " ":
-                        try:
-                            int(cols)
-                            break
-                        except ValueError:
-                            print("Cols should be an integer")
-                    board = self.random_config(int(rows), int(cols))
-                    break
-                case _:
-                    print("Input not recognized, please try again")
-        self.__grid = Grid(board)
-        print(f"Board loaded.\nNote: \u25A0 denotes a living cell and \u25A1 denotes a dead one\n{self.__grid}")
-        input("Controls:\nPress 'Q' to quit\nPress 'M' to activate manual mode\nPress 'S' to step manual mode forward\nPress 'A' to return to automatic mode\nPress 'Enter' to begin the simulation\n")
-        kb = KBHit()
-        turn = 0
-        repeat = False
-        death = False
-        while not death and not repeat:
-            self.__grid.count_living()
-            print(f"Turn: {turn}\n{self.__grid}")
-            turn += 1
-            self.__grid.update_all_neighbors()
-            self.__grid.predict_all_cells()
-            next_grid = copy.deepcopy(self.__grid)
-            next_grid.update_all_cells()
-            repeat = self.check_repeat()
-            self.add_to_prev()
-            self.__grid = next_grid
-            if self.__grid.living_cells <= 0:
-                print("All the cells died...")
-                death = True
-            if kb.kbhit():
-                c = kb.getch()
-                match c.upper():
-                    case "Q":
+        game = True
+        while game:
+            print("Welcome to the Game of Life.")
+            while board_state:= input("To begin, input 'l' to load a file, 'r' to generate a random board, or 'q' to quit.\n>") + " ":
+                match board_state.lower().strip():
+                    case "l":
+                        print("Load chosen.\nPlease enter the name of the configuration file.")
+                        files = [f[:f.find(".")].upper() for f in os.listdir("GOL_Configs") if os.path.isfile(os.path.join("GOL_Configs", f))]
+                        [print("  - " + line) for line in files]
+                        while filename := input("> ") + " ":
+                            if filename.rstrip().upper() in files:
+                                board = self.read_config(filename.rstrip())
+                                break
+                            else:
+                                print("Not a valid configuration file.")
+                        break
+                    case "r":
+                        print("Random chosen.\nPlease input your dimensions:")
+                        while rows := input("Rows:\n>") + " ":
+                            try:
+                                int(rows)
+                                break
+                            except ValueError:
+                                print("Rows should be an integer")
+                        while cols := input("Columns:\n>") + " ":
+                            try:
+                                int(cols)
+                                break
+                            except ValueError:
+                                print("Cols should be an integer")
+                        board = self.random_config(int(rows), int(cols))
+                        break
+                    case "q":
                         quit()
-                    case "M":
-                        print("Manual Mode, press 'S' to step and 'A' to return to automatic")
-                        self.__manual_mode = True
-            if self.__manual_mode:
-                while True:
-                    if kb.kbhit():
-                        c = kb.getch()
-                        match c.upper():
-                            case "Q":
-                                quit()
-                            case "A":
-                                print("Automatic Mode")
-                                self.__manual_mode = False
-                                break
-                            case "S":
-                                break
-            else:
-                time.sleep(self.__sim_speed)
+                    case _:
+                        print("Input not recognized, please try again")
+            self.__grid = Grid(board)
+            print(f"Board loaded.\nNote: \u25A0 denotes a living cell and \u25A1 denotes a dead one\n{self.__grid}")
+            input("Controls:\nPress 'Q' to quit\nPress 'M' to activate manual mode\nPress 'S' to step manual mode forward\nPress 'A' to return to automatic mode\nPress 'Enter' to begin the simulation\n")
+            kb = KBHit()
+            turn = 0
+            repeat = False
+            death = False
+            while not death and not repeat:
+                self.__grid.count_living()
+                print(f"Turn: {turn}\n{self.__grid}")
+                turn += 1
+                self.__grid.update_all_neighbors()
+                self.__grid.predict_all_cells()
+                next_grid = copy.deepcopy(self.__grid)
+                next_grid.update_all_cells()
+                repeat = self.check_repeat(turn)
+                self.add_to_prev()
+                self.__grid = next_grid
+                if self.__grid.living_cells <= 0:
+                    print(f"All the cells died...\nThe game lasted {turn} rounds")
+                    death = True
+                if kb.kbhit():
+                    c = kb.getch()
+                    match c.upper():
+                        case "Q":
+                            print(f"Quitting, game lasted {turn} rounds")
+                            quit()
+                        case "M":
+                            print("Manual Mode, press 'S' to step and 'A' to return to automatic")
+                            self.__manual_mode = True
+                if self.__manual_mode:
+                    while True:
+                        if kb.kbhit():
+                            c = kb.getch()
+                            match c.upper():
+                                case "Q":
+                                    print(f"Quitting, game lasted {turn} rounds")
+                                    quit()
+                                case "A":
+                                    print("Automatic Mode")
+                                    self.__manual_mode = False
+                                    break
+                                case "S":
+                                    break
+                else:
+                    time.sleep(self.__sim_speed)
+            while rep := input("Would you like to play again? (Y/N)\n>"):
+                match rep.strip().upper():
+                    case "Y":
+                        break
+                    case "N":
+                        quit()
+                    case _:
+                        print("Invalid input")
 
 
     @staticmethod
@@ -100,6 +113,11 @@ class GameController:
             symbol_list = []
             while line:= f.readline().strip().upper():
                 symbol_list.append([i for i in line])
+        if len(symbol_list) != rows or len(symbol_list[0]) != cols:
+            raise ValueError("Invalid configuration file, ROWS or COLS is not the correct value")
+        for i in symbol_list:
+            if len(i) == len(symbol_list[0]):
+                raise ValueError("All rows in GRID should be the same length")
         output = []
         for line in symbol_list:
             line_output = []
@@ -110,7 +128,7 @@ class GameController:
                     case "O":
                         line_output.append(False)
                     case _:
-                        raise ValueError("Invalid configuration file")
+                        raise ValueError("Invalid configuration file, GRID contains symbols other than 'x' and 'o'")
             output.append(line_output)
         return output
 
@@ -125,15 +143,12 @@ class GameController:
         else:
             self.__previous_grids.append(self.__grid)
 
-    def check_repeat(self) ->bool:
-        if self.__grid in self.__previous_grids:
-            while inp := input("Repeat detected, would you like to quit? (Y/N)\n>"):
-                match inp.rstrip().upper():
-                    case "Y":
-                        return True
-                    case "N":
-                        return False
-                    case _:
-                        print("Invalid input")
+    def check_repeat(self, turn) ->bool:
+        if self.__grid in self.__previous_grids[:-1]:
+            print(f"Ended due to repeats.\nThe game lasted {turn} rounds.")
+            return True
+        elif self.__grid == self.__previous_grids[-1] if len(self.__previous_grids) else False:
+            print(f"Ended due to stability.\nThe game lasted {turn} rounds.")
+            return True
         else:
             return False
