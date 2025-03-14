0352aedc94fed9f43e817e26b9a29566652059f1
Arcturus-The-Star <sjavery@willamette.edu>
2025-03-14 15:24:09 -0700

    implemented circular queue

--- datastructures/circularqueue.py
+++ datastructures/circularqueue.py
@@ -18,43 +18,72 @@ class CircularQueue(IQueue[T]):
                 maxsize: The maximum size of the queue
                 data_type: The type of the elements in the queue
         '''
-        raise NotImplementedError
+        self.__data_type = data_type
+        self.__max_size = maxsize
+        self.circularqueue = Array([self.__data_type() for _ in range(self.__max_size + 1)])
+        self.__front = 0
+        self.__rear = 0
 
     def enqueue(self, item: T) -> None:
-        raise NotImplementedError
+        if not isinstance(item, self.__data_type):
+            raise TypeError("Item is of invalid type")
+        elif self.full:
+            raise IndexError("Queue is full")
+        else:
+            self.circularqueue[self.__rear] = item
+            self.__rear = (self.__rear + 1) % (self.__max_size + 1)
 
     def dequeue(self) -> T:
-        raise NotImplementedError
+        if self.empty:
+            raise IndexError("Queue is empty")
+        else:
+            item = self.circularqueue[self.__front]
+            self.__front = (self.__front + 1) % (self.__max_size + 1)
+            return item
 
     def clear(self) -> None:
-        raise NotImplementedError
+        self.__front = 0
+        self.__rear = 0
 
     @property
     def front(self) -> T:
-        raise NotImplementedError
+        return self.circularqueue[self.__front]
 
     @property
     def full(self) -> bool:
-        raise NotImplementedError
+        return (self.__rear + 1) % (self.__max_size + 1) == self.__front
 
     @property
     def empty(self) -> bool:
-
-        raise NotImplementedError
+        return self.__front == self.__rear
     
     @property
     def maxsize(self) -> int:
-        raise NotImplementedError
+        return self.__max_size
+
+    @property
+    def front_pointer(self):
+        return self.__front
+
+    @property
+    def rear_pointer(self):
+        return self.__rear
 
     def __eq__(self, other: object) -> bool:
-        raise NotImplementedError   
+        if not len(self) == len(other):
+            return False
+        else:
+            equality = []
+            for i in range(len(self)):
+                equality.append((self_thing := self.circularqueue[((self.__front + i) % (self.__max_size + 1) )])== (other_thing := (other.circularqueue[(other.front_pointer + i) % (other.maxsize + 1)])))
+            return all(equality)
+
+
     
     def __len__(self) -> int:
-
-        raise NotImplementedError
+        return (self.__rear - self.__front + self.__max_size + 1) % (self.__max_size + 1)
 
     def __str__(self) -> str:
-
         return str(self.circularqueue)
 
     def __repr__(self) -> str:
